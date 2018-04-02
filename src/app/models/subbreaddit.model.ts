26d1b2beedd26e8e4a3ab9e892c9601a685d9e28
michael albers and roger yee <roger@>
2018-04-02 10:19:53 -0700

    builds new subreaddit form component

--- src/app/models/subbreaddit.model.ts
+++ src/app/models/subbreaddit.model.ts
@@ -1,11 +1,11 @@
 export class Subbreaddit {
+  public background: string;
   constructor(
     public id: string,
     public name: string,
     public title: string,
     public description: string,
     public sidebar: string,
-    public submissionText: string,
-    public backgroundImage: string
+    public submissionText: string
   ) { }
 }
